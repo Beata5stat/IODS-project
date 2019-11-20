6f935984a76e07c6462e75b687db59932aa6fc0a
Beata Anton <beata.anton@helsinki.fi>
2019-11-20 21:17:19 +0200

    Commit human

--- create_human.R
+++ create_human.R
@@ -1,3 +1,5 @@
+#Data wrangling, Beata Anton, 20.11.2019
+
 #Read the data into R
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
